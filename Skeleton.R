2f082055ee75089a15c349ff944f5907ea615840
Remo Bruhin <91737428+Teccaa@users.noreply.github.com>
2023-03-03 19:42:34 +0100

    Create Skeleton.R

---
+++ Skeleton.R
@@ -0,0 +1,14 @@
+rm(list=ls())
+
+#Set working directory
+setwd("Your\\working\\directory")
+
+
+#install the packages
+install.packages("data.table")
+install.packages("nnet")
+
+#Load the packages
+library(data.table)
+library(nnet)
+
